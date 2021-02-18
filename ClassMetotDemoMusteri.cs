ab6f77bb7305333a579501079407f0cd1d160719
tarikgosker <70004738+tarikgosker@users.noreply.github.com>
2021-02-19 01:21:30 +0300

    Create ClassMetotDemoMusteri.cs

---
+++ ClassMetotDemoMusteri.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Eposta { get; set; }
+
+    }
+}
